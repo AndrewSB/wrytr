dbf3a197c30e753af8daec48f93aef7254c99737
Andrew Breckenridge <asbreckenridge@me.com>
2015-10-24 14:48:33 -0700

    implement movie demo

--- index.ios.js
+++ index.ios.js
@@ -1,3 +1,4 @@
+/* @flow */
 /**
  * Sample React Native App
  * https://github.com/facebook/react-native
@@ -7,25 +8,26 @@
 var React = require('react-native');
 var {
   AppRegistry,
+  Image,
   StyleSheet,
   Text,
   View,
 } = React;
+var MOCKED_MOVIES_DATA = [
+	{title: 'Title', year: '2015', posters: {thumbnail: 'http://i.imgur.com/UePbdph.jpg'}}
+];
 
 var AwesomeProject = React.createClass({
   render: function() {
+	var movie = MOCKED_MOVIES_DATA[0];
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
+        <Text>{movie.title}</Text>
+		<Text>{movie.year}</Text>
+		<Image 
+			source={{uri: movie.posters.thumbnail}} 
+			style={styles.thumbnail}	
+		/>
       </View>
     );
   }
@@ -38,15 +40,9 @@ var styles = StyleSheet.create({
     alignItems: 'center',
     backgroundColor: '#F5FCFF',
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
+  thumbnail: {
+	width: 53,
+	height: 81,
   },
 });
 
