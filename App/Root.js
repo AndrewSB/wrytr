195be58ac97e365a98f5278557c5088924df01a2
Andrew Breckenridge <asbreckenridge@me.com>
2015-10-25 20:00:30 -0700

    initial view renders

---
+++ App/Root.js
@@ -0,0 +1,24 @@
+/* @flow */
+'use strict';
+
+import React from 'react-native';
+var {
+	View,	
+	Text,
+} = React;
+
+class wrytr extends React.Component {
+	getInitialState() {
+		
+	}
+
+	render() {
+		return (
+			<View>
+				<Text>yo</Text>
+			</View>
+		);
+	}
+}
+
+export default wrytr;
