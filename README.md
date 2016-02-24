947cf677302a6558ead8ed680e5564c49cb5eb01
Andrew Breckenridge <asbreckenridge@me.com>
2016-02-24 17:10:55 +0700

    clean for reswift

--- README.md
+++
@@ -1,5 +0,0 @@
-# wrytr
-A react native generic social app
-
-### For later
-- Navigation https://medium.com/the-exponent-log/routing-and-navigation-in-react-native-6b27bee39603
--- index.ios.js
+++
@@ -1,7 +0,0 @@
-/* @flow */
-'use strict';
-
-import React from 'react-native';
-import wrytr from './src/index.js';
-
-React.AppRegistry.registerComponent('wrytr', () => wrytr);
