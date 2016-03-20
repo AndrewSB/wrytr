85e12aabc6fd3c8fa91727a4c37027186ee55886
Andrew Breckenridge <asbreckenridge@me.com>
2016-03-20 14:56:16 -0700

    add Amazon and Facebook

--- wrytr/AppDelegate.swift
+++ wrytr/AppDelegate.swift
@@ -13,6 +13,7 @@ import ReSwiftRouter
 
 import Fabric
 import Crashlytics
+import AWSCognito
 
 var store = Store<State>(reducer: AppReducer(), state: nil)
 
@@ -24,7 +25,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
 
-        Fabric.with([Crashlytics.self])
+        Fabric.with([Crashlytics.self, AWSCognito.self])
         
         window = UIWindow(frame: UIScreen.mainScreen().bounds)
         
