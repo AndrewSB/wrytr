2f211ed2d92d1dcba496b0e9aadf602b4cf00ca1
Andrew Breckenridge <asbreckenridge@me.com>
2016-08-30 15:46:09 -0700

    create DismissesKeyboardOnTap.swift

---
+++ wrytr/Supporting Files/DismissesKeyboardOnTap.swift
@@ -0,0 +1,36 @@
+import UIKit
+import RxSwift
+import RxCocoa
+
+protocol DismissesKeyboardOnTap: class {
+    var disposeBag: DisposeBag { get }
+    func registerForKeyboardDismissal()
+}
+
+extension DismissesKeyboardOnTap where Self: UIView {
+    
+    func defaultRegisterForKeyboardDismissal() {
+        let tapGestureRecognizer = UITapGestureRecognizer()
+        tapGestureRecognizer.rx.event.asDriver()
+            .drive(onNext: { _ in self.endEditing(true) })
+            .addDisposableTo(disposeBag)
+    
+        addGestureRecognizer(tapGestureRecognizer)
+    }
+    
+}
+
+class ViewThatDismissesKeyboardOnTap: UIView {
+    let disposeBag = DisposeBag()
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        registerForKeyboardDismissal()
+    }
+}
+
+extension ViewThatDismissesKeyboardOnTap: DismissesKeyboardOnTap {
+    func registerForKeyboardDismissal() {
+        defaultRegisterForKeyboardDismissal()
+    }
+}
