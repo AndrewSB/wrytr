69f8a5faa46dfe45fd904c691604f02bd6135561
Andrew Breckenridge <asbreckenridge@me.com>
2016-08-31 17:43:39 -0700

    add back facebook & twitter login

--- wrytr/Common/FacebookProvider.swift
+++ wrytr/Common/FacebookProvider.swift
@@ -1,20 +1,21 @@
-//import FBSDKCoreKit
-//import FBSDKLoginKit
-//import RxSwift
-//
-//class Facebook {
-//
-//    class Provider {
-//        
-//        func login(withReadPermissions permissions: [String] = ["email", "public_profile", "user_friends"]) -> Observable<FBSDKLoginManagerLoginResult> {
-//        
-//            return ParseRxCallbacks.createWithCallback({ observer -> Void in
-//                FBSDKLoginManager.logIn(withReadPermissions: permissions, from: nil, handler: ParseRxCallbacks.rx_parseUnwrappedOptionalCallback(observer))
-//                return
-//            })
-//            
-//        }
-//        
-//    }
-//    
-//}
+import FBSDKCoreKit
+import FBSDKLoginKit
+import RxSwift
+
+class Facebook {
+
+    class Provider {
+        
+        func login(withReadPermissions permissions: [String] = ["email", "public_profile", "user_friends"]) -> Observable<FBSDKLoginManagerLoginResult> {
+        
+            return ParseRxCallbacks.createWithCallback({ observer -> Void in
+                FBSDKLoginManager().logIn(withReadPermissions: permissions, from: nil, handler: ParseRxCallbacks.rx_parseUnwrappedOptionalCallback(observer))
+                return
+            })
+            
+        }
+        
+    }
+    
+}
+
--- wrytr/Common/TwitterProvider.swift
+++ wrytr/Common/TwitterProvider.swift
@@ -1,17 +1,17 @@
-//import TwitterKit
-//import RxSwift
-//
-//extension Twitter {
-//    
-//    class Provider {
-//        
-//        func login() -> Observable<TWTRSession> {
-//            return ParseRxCallbacks.createWithCallback({ observer in
-//                Twitter.logIn(completion: ParseRxCallbacks.rx_parseUnwrappedOptionalCallback(observer))
-//                return
-//            })
-//        }
-//        
-//    }
-//    
-//}
+import TwitterKit
+import RxSwift
+
+extension Twitter {
+    
+    class Provider {
+        
+        func login() -> Observable<TWTRSession> {
+            return ParseRxCallbacks.createWithCallback({ observer in
+                Twitter.sharedInstance().logIn(completion: ParseRxCallbacks.rx_parseUnwrappedOptionalCallback(observer))
+                return
+            })
+        }
+        
+    }
+    
+}
