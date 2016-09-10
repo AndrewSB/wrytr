1c81f85a6647b279129f7bb69716d4416868a8e2
Andrew Breckenridge <asbreckenridge@me.com>
2016-09-09 18:29:02 -0700

    create empty Create & Me modules

---
+++ wrytr/Molecules/CreateViewController.swift
@@ -0,0 +1,9 @@
+//
+//  CreateViewController.swift
+//  wrytr
+//
+//  Created by Andrew Breckenridge on 9/9/16.
+//  Copyright © 2016 Andrew Breckenridge. All rights reserved.
+//
+
+import Foundation
---
+++ wrytr/Molecules/MeViewUI.swift
@@ -0,0 +1,9 @@
+//
+//  MeViewUI.swift
+//  wrytr
+//
+//  Created by Andrew Breckenridge on 9/9/16.
+//  Copyright © 2016 Andrew Breckenridge. All rights reserved.
+//
+
+import Foundation
