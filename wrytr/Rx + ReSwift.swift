e9c0bc2763bf681c7d47529b6206ef28253f62a9
Andrew Breckenridge <asbreckenridge@me.com>
2016-12-05 21:01:39 -0800

    Create Rx + ReSwift

---
+++ wrytr/Rx + ReSwift.swift
@@ -0,0 +1,42 @@
+import RxSwift
+import RxCocoa
+import ReSwift
+import Cordux
+
+/* ReactiveStoreType is a protocol that defines the reactive behaviour of a state store. */
+public protocol ReactiveStoreType {
+    associatedtype ReactiveStoreState
+    func asObservable<S: StoreSubscriber>()-> Observable<ReactiveStoreState> where S.StoreSubscriberStateType == ReactiveStoreState
+}
+
+/* Custom subscriber that forwards the next events through a PublishSubject */
+
+internal class ReactiveStoreSubscriber<S>: Cordux.SubscriberType {
+
+    internal typealias StoreSubscriberStateType = S
+    internal let subject: PublishSubject<S>
+
+    internal init() {
+        self.subject = PublishSubject()
+    }
+
+    func newState(_ state: StoreSubscriberStateType) {
+        self.subject.onNext(state)
+    }
+
+}
+
+extension Cordux.Store: ReactiveStoreType {
+
+    public typealias ReactiveStoreState = State
+
+    public func asObservable() -> Observable<ReactiveStoreState> {
+        return Observable.create({ (observer) -> Disposable in
+            let subscriber = ReactiveStoreSubscriber<ReactiveStoreState>()
+            self.subscribe(subscriber)
+            self.subscribe(subscriber)
+            return Disposables.create { self.unsubscribe(subscriber) }
+        })
+    }
+
+}
